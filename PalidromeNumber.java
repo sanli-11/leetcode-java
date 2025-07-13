9592ec3ba4c4f26582d85e358297fdd7529a45cd
SanLi <sanli.has11@gmail.com>
2025-07-13 15:08:11 +0500

    Reversed the number to check for palindrome

--- PalidromeNumber.java
+++ PalidromeNumber.java
@@ -5,6 +5,22 @@ class PalidromeNumber {
         else if (x <= 9)
             return true;
 
+        long reversed = 0;
+
+        for (long i = 1; i < x; i *= 10) {
+            long temp = x % (i * 10);
+            reversed = reversed * 10 + temp / i;
+        }
+
+        return x == reversed;
+    }
+
+    public boolean isPalindromeStringConversion(int x) {
+        if (x < 0)
+            return false;
+        else if (x <= 9)
+            return true;
+
         String str = String.valueOf(x);
 
         for (int i = 0; i < Math.floor(str.length() / 2); i++) {
@@ -21,9 +37,11 @@ class PalidromeNumber {
         boolean case1Results = p.isPalindrome(121);
         boolean case2Results = p.isPalindrome(-121);
         boolean case3Results = p.isPalindrome(10);
+        boolean case4Results = p.isPalindrome(1410110141);
 
         System.out.println(case1Results);
         System.out.println(case2Results);
         System.out.println(case3Results);
+        System.out.println(case4Results);
     }
 }
