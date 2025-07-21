a86ba36cd69d47e29f1585c94e4e2b195aed62ef
SanLi <sanli.has11@gmail.com>
2025-07-21 14:55:53 +0500

    Finding longest common prefix using nested loops

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,21 +1,17 @@
 public class LongestCommonPrefix {
   public String longestCommonPrefix(String[] strs) {
-    int shortestWordIndex = 0;
-
-    for (int i = 1; i < strs.length; i++) {
-      if (strs[i].length() < strs[shortestWordIndex].length())
-        shortestWordIndex = i;
-    }
+    if (strs.length <= 1)
+      return strs[0];
 
     String commonPrefix = "";
-    String shortestWord = strs[shortestWordIndex];
+    String firstWord = strs[0];
 
-    for (int j = 0; j < shortestWord.length(); j++) {
-      String substring = commonPrefix + shortestWord.charAt(j);
+    for (int j = 0; j < firstWord.length(); j++) {
+      for (int k = 1; k < strs.length; k++) {
+        String substring = commonPrefix + firstWord.charAt(j);
 
-      for (int k = 0; k < strs.length; k++) {
-        if (!strs[k].contains(substring))
-          break;
+        if (!strs[k].startsWith(substring))
+          return commonPrefix;
 
         if (k == strs.length - 1)
           commonPrefix = substring;
@@ -27,6 +23,9 @@ public class LongestCommonPrefix {
 
   public static void main(String[] args) {
     LongestCommonPrefix lcp = new LongestCommonPrefix();
+
+    String case0Result = lcp
+        .longestCommonPrefix(new String[] { "flower", "flower", "flower" });
     String case1Result = lcp
         .longestCommonPrefix(new String[] { "flower", "flow", "flight" });
     String case2Result = lcp
@@ -35,10 +34,13 @@ public class LongestCommonPrefix {
         .longestCommonPrefix(new String[] { "dom", "omega", "gloomy" });
     String case4Result = lcp
         .longestCommonPrefix(new String[] { "reflower", "flow", "flight" });
+    String case5Result = lcp.longestCommonPrefix(new String[] { "a" });
 
+    System.out.println(case0Result);
     System.out.println(case1Result);
     System.out.println(case2Result);
     System.out.println(case3Result);
     System.out.println(case4Result);
+    System.out.println(case5Result);
   }
 }
